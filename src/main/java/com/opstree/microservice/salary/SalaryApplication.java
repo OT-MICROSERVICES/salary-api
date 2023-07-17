59a35cc5fe3f57fa1193368f62b193f735420666
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2023-07-17 23:01:35 +0530

    Added code for spring redis integration
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>

--- src/main/java/com/opstree/microservice/salary/SalaryApplication.java
+++ src/main/java/com/opstree/microservice/salary/SalaryApplication.java
@@ -3,12 +3,46 @@ package com.opstree.microservice.salary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+import org.springframework.context.annotation.Bean;
+import com.opstree.microservice.salary.model.Employee;
+
+import java.time.Duration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 @SpringBootApplication
+@EnableCaching
 public class SalaryApplication {
+
+  @Bean
+  public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    RedisTemplate<Object, Object> template = new RedisTemplate<>();
+    template.setConnectionFactory(connectionFactory);
+    Jackson2JsonRedisSerializer<Employee> serializer = new Jackson2JsonRedisSerializer<>(Employee.class);
+    template.setDefaultSerializer(serializer);
+    return template;
+  }
+
+  @Bean
+  public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+    RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig() //
+        .prefixCacheNameWith(this.getClass().getPackageName() + ".") //
+        .entryTtl(Duration.ofSeconds(1)) //
+        .disableCachingNullValues();
+
+    return RedisCacheManager.builder(connectionFactory) //
+        .cacheDefaults(config) //
+        .build();
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalaryApplication.class, args);
 	}
-
 }
