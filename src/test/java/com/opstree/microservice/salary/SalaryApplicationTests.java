ba69efb897adbc37d9fd1b99d150aa244f4781aa
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-02-19 15:07:44 +0530

    Update SalaryApplicationTests.java

--- src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
+++ src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
@@ -3,7 +3,7 @@ package com.opstree.microservice.salary;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class SalaryApplicationTests {
 
 	@Test
